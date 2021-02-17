ee84c000eeac1fc1175ccae7ec0654cb060d0a30
BasiliskOps <noro.nero@gmail.com>
2021-02-16 23:32:55 -0600

    wrote function to implement game logic

--- app.js
+++ app.js
@@ -29,3 +29,31 @@ let p2Deck = devideDeck.slice(26, 52)
 console.log(p2Deck)
 // ^^ this function then shuffles and divides the deck into two halves
 
+function gameStart() {
+    for (let i = 0, j = 0; i < 26, j < 26; i++, j++) {
+      if (p1Deck[i].Value > p2Deck[j].Value) {
+        console.log(
+          "Player 1: " +
+            p1Deck[i].Value +
+            p1Deck[i].Suit +
+            " " +
+            "Player 2: " +
+            p2Deck[j].Value +
+            p2Deck[j].Suit
+        )
+        console.log("Player 1 Wins!")
+         p1Deck.push(p1Deck[i])
+         p1Deck.push(p2Deck[j])
+         p2Deck.pop()
+         console.log(
+           "Player 1 has: " +
+             p1Deck.length +
+             "cards" +
+             "  " +
+            "Player 2 has: " +
+          p2Deck.length +
+          "cards"
+        )
+// ^^ this function begins the game logic and uses a for loop to state conditions for player 1 to win  
+
+ 
